565e5f8fc2e7d28b78fccab20ed0cd5079a1375f
Thinnapat-Prasopakarakit-hybytes <thinnapat.prasopakarakit@hybytes.co.uk>
2024-12-03 09:31:43 +0000

    Add Top Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="hero_area">
+      <header className="header_section">
+        <div className="header_top">
+          <div className="container-fluid">
+            <div className="contact_nav">
+              <Link to="/">
+                <FaPhoneAlt
+                  className="contact_nav-icon"
+                  style={{ marginRight: "5px" }}
+                />
+                <span>Call : +01 123455678990</span>
+              </Link>
+              <Link to="/">
+                <FaEnvelope className="contact_nav-icon" />
+                <span>Email : demo@gmail.com</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="header_bottom">
+          <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg custom_nav-container">
+              <a className="navbar-brand" href="index.html">
+                <span>Inance</span>
+              </a>
+
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className=""> </span>
+              </button>
+
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="index.html">
+                      Home <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li className="nav-item active">
+                    <a className="nav-link" href="about.html">
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="service.html">
+                      Services
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="contact.html">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
