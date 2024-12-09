dd4c805918ab3094e8cd1793c34b2118581243f4
Thinnapat-Prasopakarakit-hybytes <thinnapat.prasopakarakit@hybytes.co.uk>
2024-12-09 11:07:52 +0000

    Convert static list to dynamic list in ServiceComponent

--- src/components/ServiceComponent/ServiceComponent.jsx
+++ src/components/ServiceComponent/ServiceComponent.jsx
@@ -6,6 +6,27 @@ import s3 from "../../assets/images/s3.png";
 import { Link } from "react-router-dom";
 
 const ServiceComponent = () => {
+  const services = [
+    {
+      image: s1,
+      title: "Maintenance",
+      description:
+        "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal",
+    },
+    {
+      image: s2,
+      title: "Electrical",
+      description:
+        "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal",
+    },
+    {
+      image: s3,
+      title: "Plumbing",
+      description:
+        "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal",
+    },
+  ];
+
   return (
     <section className="service_section layout_padding">
       <div className="container">
@@ -13,48 +34,19 @@ const ServiceComponent = () => {
           <h2>Our Services</h2>
         </div>
         <div className="row">
-          <div className="col-sm-6 col-md-4 mx-auto px-3">
-            <div className="box">
-              <div className="img-box">
-                <img src={s1} alt="s1" />
-              </div>
-              <div className="detail-box">
-                <h5>Maintenance</h5>
-                <p>
-                  when looking at its layout. The point of using Lorem Ipsum is
-                  that it has a more-or-less normal
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 col-md-4 mx-auto px-3">
-            <div className="box">
-              <div className="img-box">
-                <img src={s2} alt="s2" />
-              </div>
-              <div className="detail-box">
-                <h5>Electrical</h5>
-                <p>
-                  when looking at its layout. The point of using Lorem Ipsum is
-                  that it has a more-or-less normal
-                </p>
+          {services.map((service, idx) => (
+            <div className="col-sm-6 col-md-4 mx-auto px-3" key={idx}>
+              <div className="box">
+                <div className="img-box">
+                  <img src={service.image} alt={service.title} />
+                </div>
+                <div className="detail-box">
+                  <h5>{service.title}</h5>
+                  <p>{service.description}</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 col-md-4 mx-auto px-3">
-            <div className="box">
-              <div className="img-box">
-                <img src={s3} alt="s3" />
-              </div>
-              <div className="detail-box">
-                <h5>Plumbing</h5>
-                <p>
-                  when looking at its layout. The point of using Lorem Ipsum is
-                  that it has a more-or-less normal
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
         <div className="btn-box">
           <Link to="/"> View More </Link>
