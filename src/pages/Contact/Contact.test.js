bb219c58606089264661b2b3d86558d77800a20d
Thinnapat-Prasopakarakit-hybytes <thinnapat.prasopakarakit@hybytes.co.uk>
2024-12-16 10:35:05 +0000

    Add unit tests for Contact page

--- src/pages/Contact/Contact.test.js
+++ src/pages/Contact/Contact.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import Home from "./Home";
+import Contact from "./Contact";
 import { APIProvider } from "@vis.gl/react-google-maps";
 
 jest.mock("@vis.gl/react-google-maps", () => ({
@@ -20,45 +20,34 @@ jest.mock("@vis.gl/react-google-maps", () => ({
     />
   ),
 }));
-
-const renderHome = () => {
+const renderContact = () => {
   return render(
     <BrowserRouter>
       <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-        <Home />
+        <Contact />
       </APIProvider>
     </BrowserRouter>
   );
 };
 
-describe("Home Page", () => {
-  it("should render all components", () => {
-    renderHome();
-
-    // Feature component
-    expect(screen.getByText("Repair")).toBeInTheDocument();
-    expect(screen.getByText("Improve")).toBeInTheDocument();
-    expect(screen.getByText("Maintain")).toBeInTheDocument();
-
-    //  About component
-    expect(screen.getByText("About us")).toBeInTheDocument();
-
-    // Professional component
-    expect(screen.getByText(/We Provide Professional/i)).toBeInTheDocument();
-
-    // Service component
-    expect(screen.getByText("Our Services")).toBeInTheDocument();
-
-    // Client component
-    expect(screen.getByText("What Our Clients Say")).toBeInTheDocument();
-
-    // Contact component
+describe("Contact Page", () => {
+  it("should render contact form", () => {
+    renderContact();
     expect(screen.getByText("Contact Us")).toBeInTheDocument();
-    expect(screen.getByTestId("mock-map")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Name")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Phone Number")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Email")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Message")).toBeInTheDocument();
+  });
+
+  it("should render map component", () => {
+    renderContact();
+    const mapElement = screen.getByTestId("mock-map");
+    expect(mapElement).toBeInTheDocument();
   });
 
   it("should match snapshot", () => {
-    const { asFragment } = renderHome();
+    const { asFragment } = renderContact();
     expect(asFragment()).toMatchSnapshot();
   });
 });
